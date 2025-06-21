0d9a5d243dd190ff780ffa41149d57e145c77bb3
Dominica Wannenburg <dominicawan@gmail.com>
2025-06-21 16:04:16 +0200

    Basic library folder structure set up + readme update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es'],
+      name: 'SednaSettingsUi',
+      fileName: 'sedna-settings-ui',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'React-dom',
+        },
+      },
+    },
+  },
 })
